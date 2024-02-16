a04ad077928a858793c9f69b9ee347d634c3729d
Marla Ingridh <marla.ingridh@gmail.com>
2024-02-16 08:14:09 -0400

    Melhorando a formatação da saída dos dados na classe MinhasPreferidas.

--- src/br/com/alura/minhasmusicas/modelos/MinhasPreferidas.java
+++ src/br/com/alura/minhasmusicas/modelos/MinhasPreferidas.java
@@ -3,10 +3,10 @@ package br.com.alura.minhasmusicas.modelos;
 public class MinhasPreferidas {
     public void inclui(Audio audio){
         if(audio.getClassificacao() >= 9){
-            System.out.println(audio.getTitulo()+" é considerado sucesso absoluto"+
+            System.out.println("[ "+ audio.getTitulo()+" ] é considerado sucesso absoluto"+
                     "e preferido por todos!");
         }else {
-            System.out.println(audio.getTitulo()+ " também é um dos que todo mundo esta curtindo.");
+            System.out.println("[ " + audio.getTitulo()+ " ] também é um dos que todo mundo esta curtindo.");
         }
     }
 }
